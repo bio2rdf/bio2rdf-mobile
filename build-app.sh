c54a4f5b67517ffd28f21c33616c1bb4a926c4cf
Maxime Déraspe <maximilien1er@gmail.com>
2014-04-22 23:16:30 -0400

    added build-app.sh to build the app automatically
    Signed-off-by: Maxime Déraspe <maximilien1er@gmail.com>

---
+++ build-app.sh
@@ -0,0 +1,5 @@
+ionic build android
+cd platforms/android/ant-build/
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../../../../keystore/bio2rdf.keystore Bio2RDFmobile-debug.apk Bio2RDF
+zipalign -v 4 Bio2RDFmobile-debug.apk $1
+
