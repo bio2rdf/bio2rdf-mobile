e306886103bcca17d2af095b99a2924d51ff0e45
JFRheault <jf.rheault.1@gmail.com>
2014-05-02 11:51:42 -0400

    Describe pubmed in progress
    Signed-off-by: JFRheault <jf.rheault.1@gmail.com>

--- www/js/dbcontrollers.js
+++ www/js/dbcontrollers.js
@@ -1,4 +1,4 @@
-var module = angular.module('starter.dbcontrollers', []);
+var module = angular.module('starter.dbcontrollers', [])
 
 // OBO controller
 module.controller('OboCtrl', function($scope, $stateParams, $ionicSideMenuDelegate, $timeout, $ionicLoading, Queryer, ProcessGraph, DatasetStore, QuickLinks, Utilities, FavoriteService) {
@@ -13,7 +13,6 @@ module.controller('OboCtrl', function($scope, $stateParams, $ionicSideMenuDelega
     var main = idList[$stateParams.uri]
 
     $scope.main = main;
-    $scope.image = 
 
     $scope.title = Utilities.capitalize(main["rdfs:label"])
     $scope.obodef = main["obolibrary:IAO_0000115"]
@@ -106,22 +105,7 @@ module.controller('OboCtrl', function($scope, $stateParams, $ionicSideMenuDelega
   $scope.unSetDragging = function () {
     $ionicSideMenuDelegate.canDragContent(false);
     $timeout(reDragSideMenus,3000);
-
-ller('PubmedCtrl', function($scope, $stateParams, Queryer, ProcessGraph) {
-
-  $scope.uri = $stateParams.uri;
-  var endpoint = $stateParams.endpoint;
-  Utilities.grepDBfromURI($scope.uri);
-
-  Queryer.setQuery('pubmed','describe','json-ld', {"uri" : $scope.uri});
-  Queryer.getJson().success(function(data){
-    var idList=ProcessGraph.graph(data);
-    var main = idList[$stateParams.uri]
-    _.each(main["pubmed_vocabulary:author"], function(elem) {
-      $scope.pmauthors.push(idList[elem["@id"]]);
-      });
-
-    });
+  }
 
 });
 
@@ -135,45 +119,12 @@ module.controller('PubmedCtrl', function($scope, $stateParams, $ionicSideMenuDel
     // Query the graph data from mobile.bio2rdf.org
     Queryer.setQuery('pubmed' ,'describebeta', 'json-ld', {"uri" : $scope.uri});
     Queryer.getJson().success(function(data){
-      var idList=ProcessGraph.graph(data);
-      var main = idList[$stateParams.uri]
-
       /*$scope.title = Utilities.capitalize(main["rdfs:label"])*/
       /*$scope.dbdescription = main["dcterms:description"]["@value"];*/
-      $scope.main=main;
-      $scope.idList=idList;
-
-      $scope.title = Utilities.capitalize(main["rdfs:label"]);
-
-      $scope.obosuperclasses = []
-      _.each(main["rdfs:subClassOf"], function(elem) {
-        if (typeof elem == "string") {
-        /*if (idList[elem].substring(0,1) != "_") {*/
-        $scope.obosuperclasses.push(idList[elem])
-        /*}*/
-        } else {
-        /*if {idList[elem["@id"]]}*/
-        if (idList[elem["@id"]] != undefined) {
-        $scope.obosuperclasses.push(idList[elem["@id"]]);
-        }
-        }
-        });
-
-    $scope.obosubclasses = []
-      _.each(idList, function(elem) {
-          if (elem["rdfs:subClassOf"] != undefined) {
-          if (elem["rdfs:subClassOf"]["@id"] == $stateParams.uri ) {
-          $scope.obosubclasses.push(idList[elem["@id"]])
-          }
-          }
-          });
-
-    $scope.xrefs = []
-      if (main["oboInOwl:hasDbXref"] != undefined) {
-        _.each(main["oboInOwl:hasDbXref"], function(elem) {
-            $scope.xrefs.push(elem)
-            });
-      }
+      $scope.data=data;
+      console.log(data);
+      $scope.title = Utilities.capitalize(data["rdfs:label"]);
+      
 
 
     // Bookmark status and saving -----
@@ -286,6 +237,16 @@ module.controller('DrugBankCtrl', function($scope, $stateParams, $ionicSideMenuD
       });
     }
 
+    // Fetch Image from Wiki
+    /*ProcessGraph.getWikiImageUri($scope.uri).then(function(promise){*/
+    /*$scope.image = promise.data["http://xmlns.com/foaf/0.1/depiction"]["@id"];*/
+    /*});*/
+
+    /*if ($scope.image == undefined){*/
+    /*ProcessGraph.getWikiImageLabel($scope.title).then(function(promise){*/
+    /*$scope.image = promise.data["http://xmlns.com/foaf/0.1/depiction"]["@id"];*/
+    /*});*/
+    /*}*/
 
     // Bookmark status and saving -----
     function bookmarkLookUpCall (tx, results) {
