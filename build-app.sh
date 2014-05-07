6507b2822c8b92414bba10d2b0a1e9dd2f247b56
Maxime Déraspe <maximilien1er@gmail.com>
2014-05-06 20:54:04 -0400

    removed build-app.sh file
    Signed-off-by: Maxime Déraspe <maximilien1er@gmail.com>

--- build-app.sh
+++
@@ -1,5 +0,0 @@
-ionic build android
-cd platforms/android/ant-build/
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../../../../keystore/bio2rdf.keystore Bio2RDFmobile-debug.apk Bio2RDF
-zipalign -v 4 Bio2RDFmobile-debug-unaligned.apk $1
-
